fcb2a6233bacb8f36ee6743cf169073a7a0e42a2
valtyr <bloomquist.robert@gmail.com>
2011-10-13 09:26:40 -0400

    fixed arrays not being initialized before shuffling

--- smooth/smooth_test.go
+++ smooth/smooth_test.go
@@ -1,4 +1,4 @@
-package smoothsort_test
+package smooth
 
 import (
 	"math"
@@ -6,7 +6,13 @@ import (
 	"testing"
 )
 
-import . "github.com/runningwild/sorts/smoothsort"
+import (
+	"github.com/runningwild/sorts/smooth"
+)
+
+/*
+ * You do not need to edit anything below here.
+ */
 
 var ints = [...]int{74, 59, 238, -784, 9845, 959, 905, 0, 0, 42, 7586, -5467984, 7586}
 var float64s = [...]float64{74.3, 59.0, math.Inf(1), 238.2, -784.0, 2.3, math.NaN(), math.NaN(), math.Inf(-1), 9845.768, -959.7485, 905, 7.8, 7.8}
@@ -14,9 +20,9 @@ var strings = [...]string{"", "Hello", "foo", "bar", "foo", "f00", "%*&^*&^&", "
 
 func TestSortIntSlice(t *testing.T) {
 	data := ints
-	a := IntSlice(data[0:])
-	Sort(a)
-	if !IsSorted(a) {
+	a := smooth.IntSlice(data[0:])
+	smooth.Sort(a)
+	if !smooth.IsSorted(a) {
 		t.Errorf("sorted %v", ints)
 		t.Errorf("   got %v", data)
 	}
@@ -24,9 +30,9 @@ func TestSortIntSlice(t *testing.T) {
 
 func TestSortFloat64Slice(t *testing.T) {
 	data := float64s
-	a := Float64Slice(data[0:])
-	Sort(a)
-	if !IsSorted(a) {
+	a := smooth.Float64Slice(data[0:])
+	smooth.Sort(a)
+	if !smooth.IsSorted(a) {
 		t.Errorf("sorted %v", float64s)
 		t.Errorf("   got %v", data)
 	}
@@ -34,9 +40,9 @@ func TestSortFloat64Slice(t *testing.T) {
 
 func TestSortStringSlice(t *testing.T) {
 	data := strings
-	a := StringSlice(data[0:])
-	Sort(a)
-	if !IsSorted(a) {
+	a := smooth.StringSlice(data[0:])
+	smooth.Sort(a)
+	if !smooth.IsSorted(a) {
 		t.Errorf("sorted %v", strings)
 		t.Errorf("   got %v", data)
 	}
@@ -44,8 +50,8 @@ func TestSortStringSlice(t *testing.T) {
 
 func TestInts(t *testing.T) {
 	data := ints
-	Ints(data[0:])
-	if !IntsAreSorted(data[0:]) {
+	smooth.Ints(data[0:])
+	if !smooth.IntsAreSorted(data[0:]) {
 		t.Errorf("sorted %v", ints)
 		t.Errorf("   got %v", data)
 	}
@@ -53,8 +59,8 @@ func TestInts(t *testing.T) {
 
 func TestFloat64s(t *testing.T) {
 	data := float64s
-	Float64s(data[0:])
-	if !Float64sAreSorted(data[0:]) {
+	smooth.Float64s(data[0:])
+	if !smooth.Float64sAreSorted(data[0:]) {
 		t.Errorf("sorted %v", float64s)
 		t.Errorf("   got %v", data)
 	}
@@ -62,7 +68,7 @@ func TestFloat64s(t *testing.T) {
 
 func TestStrings(t *testing.T) {
 	data := strings
-	Strings(data[0:])
+	smooth.Strings(data[0:])
 	if !StringsAreSorted(data[0:]) {
 		t.Errorf("sorted %v", strings)
 		t.Errorf("   got %v", data)
@@ -78,291 +84,308 @@ func TestSortLarge_Random(t *testing.T) {
 	for i := 0; i < len(data); i++ {
 		data[i] = rand.Intn(100)
 	}
-	if IntsAreSorted(data) {
+	if smooth.IntsAreSorted(data) {
 		t.Fatalf("terrible rand.rand")
 	}
 	Ints(data)
-	if !IntsAreSorted(data) {
+	if !smooth.IntsAreSorted(data) {
 		t.Errorf("sort didn't sort - 1M ints")
 	}
 }
 
-func shuffle(v []int) {
+func shuffle(data []int) {
 	var n int
-	for i := len(v)-1; i > 0; i-- {
+	for i := len(data)-1; i > 0; i-- {
 		n = rand.Intn(i)
 		v[i], v[n] = v[n], v[i]
 	}
 }
 
-func partialShuffle(v []int, n int) {
-	for i := len(v)-1; n > 0; i-- {
-		r := rand.Intn(i)
-		v[i], v[r] = v[r], v[i]
+func random(data []int) {
+	for i := range data {
+		data[i] = rand.Int()
+	}
+}
+
+func reverse(data []int) {
+	n := len(data) - 1
+	for i := range data {
+		data[i] = n
 		n--
 	}
 }
 
-func reverse(v []int) {
-	for i := range v {
-		v[i] = len(v) - i - 1
-	}
-}
-
-func inOrder(v []int) {
-	for i := range v {
-		v[i] = i
+func inOrder(data []int) {
+	for i := range data {
+		data[i] = i
 	}
 }
 
 func BenchmarkSorted10(b *testing.B) {
-	v := make([]int, 10)
-	inOrder(v)
+	b.StopTimer()
+	data := make([]int, 10)
+	inOrder(data)
+	b.StartTimer()
 	for i := 0; i < b.N; i++ {
-		Ints(v)
+		smooth.Ints(data)
 	}
 }
 
-func BenchmarkMostlySorted10(b *testing.B) {
+func BenchmarkRandom10(b *testing.B) {
 	b.StopTimer()
-	v := make([]int, 10)
-	inOrder(v)
+	data := make([]int, 10)
+	inOrder(data)
 	for i := 0; i < b.N; i++ {
-		partialShuffle(v, 5)
+		random(data)
 		b.StartTimer()
-		Ints(v)
+		smooth.Ints(data)
 		b.StopTimer()
 	}
 }
 
 func BenchmarkShuffled10(b *testing.B) {
 	b.StopTimer()
-	v := make([]int, 10)
+	data := make([]int, 10)
+	inOrder(data)
 	for i := 0; i < b.N; i++ {
-		shuffle(v)
+		shuffle(data)
 		b.StartTimer()
-		Ints(v)
+		smooth.Ints(data)
 		b.StopTimer()
 	}
 }
 
 func BenchmarkReversed10(b *testing.B) {
 	b.StopTimer()
-	v := make([]int, 10)
+	data := make([]int, 10)
 	for i := 0; i < b.N; i++ {
-		reverse(v)
+		reverse(data)
 		b.StartTimer()
-		Ints(v)
+		smooth.Ints(data)
 		b.StopTimer()
 	}
 }
 
 func BenchmarkSorted100(b *testing.B) {
-	v := make([]int, 100)
-	inOrder(v)
+	b.StopTimer()
+	data := make([]int, 100)
+	inOrder(data)
+	b.StartTimer()
 	for i := 0; i < b.N; i++ {
-		Ints(v)
+		smooth.Ints(data)
 	}
 }
 
-func BenchmarkMostlySorted100(b *testing.B) {
+func BenchmarkRandom100(b *testing.B) {
 	b.StopTimer()
-	v := make([]int, 100)
-	inOrder(v)
+	data := make([]int, 100)
+	inOrder(data)
 	for i := 0; i < b.N; i++ {
-		partialShuffle(v, 5)
+		random(data)
 		b.StartTimer()
-		Ints(v)
+		smooth.Ints(data)
 		b.StopTimer()
 	}
 }
 
 func BenchmarkShuffled100(b *testing.B) {
 	b.StopTimer()
-	v := make([]int, 100)
+	data := make([]int, 100)
+	inOrder(data)
 	for i := 0; i < b.N; i++ {
-		shuffle(v)
+		shuffle(data)
 		b.StartTimer()
-		Ints(v)
+		smooth.Ints(data)
 		b.StopTimer()
 	}
 }
 
 func BenchmarkReversed100(b *testing.B) {
 	b.StopTimer()
-	v := make([]int, 100)
+	data := make([]int, 100)
 	for i := 0; i < b.N; i++ {
-		reverse(v)
+		reverse(data)
 		b.StartTimer()
-		Ints(v)
+		smooth.Ints(data)
 		b.StopTimer()
 	}
 }
 
 func BenchmarkSorted1k(b *testing.B) {
-	v := make([]int, 1000)
-	inOrder(v)
+	b.StopTimer()
+	data := make([]int, 1000)
+	inOrder(data)
+	b.StartTimer()
 	for i := 0; i < b.N; i++ {
-		Ints(v)
+		smooth.Ints(data)
 	}
 }
 
-func BenchmarkMostlySorted1k(b *testing.B) {
+func BenchmarkRandom1k(b *testing.B) {
 	b.StopTimer()
-	v := make([]int, 1000)
-	inOrder(v)
+	data := make([]int, 1000)
 	for i := 0; i < b.N; i++ {
-		partialShuffle(v, 5)
+		random(data)
 		b.StartTimer()
-		Ints(v)
+		smooth.Ints(data)
 		b.StopTimer()
 	}
 }
 
 func BenchmarkShuffled1k(b *testing.B) {
 	b.StopTimer()
-	v := make([]int, 1000)
+	data := make([]int, 1000)
+	inOrder(data)
 	for i := 0; i < b.N; i++ {
-		shuffle(v)
+		shuffle(data)
 		b.StartTimer()
-		Ints(v)
+		smooth.Ints(data)
 		b.StopTimer()
 	}
 }
 
 func BenchmarkReversed1k(b *testing.B) {
 	b.StopTimer()
-	v := make([]int, 1000)
+	data := make([]int, 1000)
 	for i := 0; i < b.N; i++ {
-		reverse(v)
+		reverse(data)
 		b.StartTimer()
-		Ints(v)
+		smooth.Ints(data)
 		b.StopTimer()
 	}
 }
 
 func BenchmarkSorted10k(b *testing.B) {
-	v := make([]int, 10000)
-	inOrder(v)
+	b.StopTimer()
+	data := make([]int, 10000)
+	inOrder(data)
+	b.StartTimer()
 	for i := 0; i < b.N; i++ {
-		Ints(v)
+		smooth.Ints(data)
 	}
 }
 
-func BenchmarkMostlySorted10k(b *testing.B) {
+func BenchmarkRandom10k(b *testing.B) {
 	b.StopTimer()
-	v := make([]int, 10000)
-	inOrder(v)
+	data := make([]int, 10000)
+	inOrder(data)
 	for i := 0; i < b.N; i++ {
-		partialShuffle(v, 5)
+		random(data)
 		b.StartTimer()
-		Ints(v)
+		smooth.Ints(data)
 		b.StopTimer()
 	}
 }
 
 func BenchmarkShuffled10k(b *testing.B) {
 	b.StopTimer()
-	v := make([]int, 10000)
+	data := make([]int, 10000)
+	inOrder(data)
 	for i := 0; i < b.N; i++ {
-		shuffle(v)
+		shuffle(data)
 		b.StartTimer()
-		Ints(v)
+		smooth.Ints(data)
 		b.StopTimer()
 	}
 }
 
 func BenchmarkReversed10k(b *testing.B) {
 	b.StopTimer()
-	v := make([]int, 10000)
+	data := make([]int, 10000)
 	for i := 0; i < b.N; i++ {
-		reverse(v)
+		reverse(data)
 		b.StartTimer()
-		Ints(v)
+		smooth.Ints(data)
 		b.StopTimer()
 	}
 }
 
 func BenchmarkSorted100k(b *testing.B) {
-	v := make([]int, 100000)
-	inOrder(v)
+	b.StopTimer()
+	data := make([]int, 100000)
+	inOrder(data)
+	b.StartTimer()
 	for i := 0; i < b.N; i++ {
-		Ints(v)
+		smooth.Ints(data)
 	}
 }
 
-func BenchmarkMostlySorted100k(b *testing.B) {
+func BenchmarkRandom100k(b *testing.B) {
 	b.StopTimer()
-	v := make([]int, 100000)
-	inOrder(v)
+	data := make([]int, 100000)
+	inOrder(data)
 	for i := 0; i < b.N; i++ {
-		partialShuffle(v, 5)
+		random(data)
 		b.StartTimer()
-		Ints(v)
+		smooth.Ints(data)
 		b.StopTimer()
 	}
 }
 
 func BenchmarkShuffled100k(b *testing.B) {
 	b.StopTimer()
-	v := make([]int, 100000)
+	data := make([]int, 100000)
+	inOrder(data)
 	for i := 0; i < b.N; i++ {
-		shuffle(v)
+		shuffle(data)
 		b.StartTimer()
-		Ints(v)
+		smooth.Ints(data)
 		b.StopTimer()
 	}
 }
 
 func BenchmarkReversed100k(b *testing.B) {
 	b.StopTimer()
-	v := make([]int, 100000)
+	data := make([]int, 100000)
 	for i := 0; i < b.N; i++ {
-		reverse(v)
+		reverse(data)
 		b.StartTimer()
-		Ints(v)
+		smooth.Ints(data)
 		b.StopTimer()
 	}
 }
 
 func BenchmarkSorted1M(b *testing.B) {
-	v := make([]int, 1000000)
-	inOrder(v)
+	b.StopTimer()
+	data := make([]int, 1000000)
+	inOrder(data)
+	b.StartTimer()
 	for i := 0; i < b.N; i++ {
-		Ints(v)
+		smooth.Ints(data)
 	}
 }
 
-func BenchmarkMostlySorted1M(b *testing.B) {
+func BenchmarkRandom1M(b *testing.B) {
 	b.StopTimer()
-	v := make([]int, 1000000)
-	inOrder(v)
+	data := make([]int, 1000000)
+	inOrder(data)
 	for i := 0; i < b.N; i++ {
-		partialShuffle(v, 5)
+		random(data)
 		b.StartTimer()
-		Ints(v)
+		smooth.Ints(data)
 		b.StopTimer()
 	}
 }
 
 func BenchmarkShuffled1M(b *testing.B) {
 	b.StopTimer()
-	v := make([]int, 1000000)
+	data := make([]int, 1000000)
+	inOrder(data)
 	for i := 0; i < b.N; i++ {
-		shuffle(v)
+		shuffle(data)
 		b.StartTimer()
-		Ints(v)
+		smooth.Ints(data)
 		b.StopTimer()
 	}
 }
 
 func BenchmarkReversed1M(b *testing.B) {
 	b.StopTimer()
-	v := make([]int, 1000000)
+	data := make([]int, 1000000)
 	for i := 0; i < b.N; i++ {
-		reverse(v)
+		reverse(data)
 		b.StartTimer()
-		Ints(v)
+		smooth.Ints(data)
 		b.StopTimer()
 	}
 }
